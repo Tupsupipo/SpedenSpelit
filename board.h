1eceb7563eae69338afb6925982951eff9445379
Tupsupipo <jussi.saarensalmi@gmail.com>
2026-09-13 18:10:25 +0300

    Alustan tunnistus pitäisi toimia

--- board.h
+++ board.h
@@ -4,18 +4,22 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-// Määritetään onko käytössä arduino UNO vaiko MEGA ja valitaan käytettävät pinnit sen mukaan
+// Määritetään onko käytössä arduino UNO vaiko MEGA. Määritetään rekisterit ja valitaan käytettävät pinnit sen mukaan
 
 #if defined(ARDUINO_AVR_MEGA2560)
 #define BOARD_NAME "MEGA"
 const int pins[] = {50, 51, 52, 53};
-//  Mieti mitä näille. Samanlainen systeemi taidetaan tarvia .cpp PCICR = PCICR_SETTING? .h CONST PCICR_SETTING = B00000001;  ??
-//  PCICR = B00000001;    // PCIE0
-//  PCMSK0 = B00001111;   // PCINT0-PCINT3 == pinnit 50-53 
+//  Näppäin asetukset MEGA:lle.
+const uint8_t PCICR_SETTING = B00000001;    // (1 << PCIE0)      Sama molemmissa == sama keskeytyspalvelin
+const uint8_t PCMSK0_SETTING = B00001111;   // PCINT0-PCINT3 == pinnit 50-53.     PCMSK2 MEGASSA sattuu analogiportteihin, joten käytetään mielummin PCMSK0
 
-#elif defined(ARDUINO_AVR_UNO) // TODO: kattele UNOa ja päivitä tiedot
+#elif defined(ARDUINO_AVR_UNO) 
 #define BOARD_NAME "UNO"
-const int pins[] = {2, 3, 4, 5};
+const int pins[] = {10, 11, 12, 13};
+// Näppäin asetukset UNO:lle. 
+const uint8_t PCICR_SETTING = B0000001;     // (1 << PCIE0)
+const uint8_t PCMSK0_SETTING = B00111100;   // PCINT2-PCINT5 == pinnit 10-13
+
 
 #else
 #error "Laitetta ei tunnistettu!"
